d910cf0a67c2401a67611b7a489016ca955cafa4
Personal-IliyanDimov <ilianmd@abv.bg>
2020-11-08 19:07:38 +0200

    Initial project structure - part3

---
+++ src/main/java/org/imd/cqrs/sample1/qs/CqrsSample1QsApplication.java
@@ -0,0 +1,13 @@
+package org.imd.cqrs.sample1.qs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsSample1QsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsSample1QsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/imd/cqrs/sample1/qs/CqrsSample1QsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imd.cqrs.sample1.qs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsSample1QsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
